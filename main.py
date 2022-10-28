13b487cc23a6593a50dadac4b6f051e912859482
Karol Wiśniewski <karol.wisniewski2901@gmail.com>
2022-10-28 18:59:14 +0100

    point going down

--- main.py
+++ main.py
@@ -9,12 +9,23 @@ class Tetris:
         self.black=(0,0,0)
         self.white=(255,255,255)
         self.gray=(105,105,105)
+        self.red=(220,20,60)
         self.square_width, self.square_height = 30,30 
         self.margin=1
         self.time=0
         self.window = pygame.display.set_mode((WIDTH,HEIGHT))
         self.font = pygame.font.SysFont('Comic Sans MS', 30)
-        self.matrix = [[0]*10]*20
+        self.matrix = [[0]*10 for _ in range(20)]
+        self.point = [-1,-1]
+    
+    def event_per_sec(self):
+        self.time+=1
+        try:
+            self.point[1]+=1
+            self.matrix[self.point[1]][5]=1
+            self.matrix[self.point[1]-1][5]=0
+        except:
+            pass
 
     def draw(self):
         self.window.fill(self.black)
@@ -22,6 +33,13 @@ class Tetris:
             for column in range(10):
                 if (self.matrix[row][column]==0):
                     pygame.draw.rect(self.window,(self.gray),[
+                    column*(self.square_height+self.margin),
+                    row*(self.square_height+self.margin),
+                    self.square_width,
+                    self.square_height
+                    ])
+                elif (self.matrix[row][column]==1):
+                    pygame.draw.rect(self.window,(self.red),[
                     column*(self.square_height+self.margin),
                     row*(self.square_height+self.margin),
                     self.square_width,
@@ -43,7 +61,7 @@ class Tetris:
                 if event.type == pygame.QUIT:
                     run = False
                 if event.type == pygame.USEREVENT:
-                    self.time+=1
+                    self.event_per_sec()
 
             self.draw()
             
