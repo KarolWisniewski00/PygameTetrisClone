c89b7c899d1dd2ce12a4762c7c92118701d8f1ad
Karol Wiśniewski <karol.wisniewski2901@gmail.com>
2022-10-29 12:25:31 +0100

    first basic working

--- main.py
+++ main.py
@@ -15,40 +15,60 @@ class Tetris:
         self.time=0
         self.window = pygame.display.set_mode((WIDTH,HEIGHT))
         self.font = pygame.font.SysFont('Comic Sans MS', 30)
-        self.matrix = [[0]*10 for _ in range(20)]
-        self.point = [-1,-1]
+        self.matrix_width = 10
+        self.matrix_height = 20
+        self.matrix = [[0]*self.matrix_width for _ in range(self.matrix_height)]
+        self.position = [4,0]
+
+    def move_down_tetrimino(self):
+        try:
+            if self.matrix[self.position[1]+1][self.position[0]] == 0:
+                self.position[1]+=1
+            else:
+                self.matrix[self.position[1]][self.position[0]] = 1
+                self.position = [4,0]
+        except:
+            self.matrix[self.position[1]][self.position[0]] = 1
+            self.position = [4,0]
     
     def event_per_sec(self):
         self.time+=1
-        try:
-            self.point[1]+=1
-            self.matrix[self.point[1]][5]=1
-            self.matrix[self.point[1]-1][5]=0
-        except:
-            pass
+        self.move_down_tetrimino()
 
-    def draw(self):
+    def draw_background(self):
         self.window.fill(self.black)
-        for row in range(20):
-            for column in range(10):
-                if (self.matrix[row][column]==0):
+        for row in range(self.matrix_height):
+            for column in range(self.matrix_width):
+                if self.matrix[row][column]==0:
                     pygame.draw.rect(self.window,(self.gray),[
                     column*(self.square_height+self.margin),
                     row*(self.square_height+self.margin),
                     self.square_width,
                     self.square_height
                     ])
-                elif (self.matrix[row][column]==1):
+                elif self.matrix[row][column]==1:
                     pygame.draw.rect(self.window,(self.red),[
                     column*(self.square_height+self.margin),
                     row*(self.square_height+self.margin),
                     self.square_width,
                     self.square_height
                     ])
-                
+
         time = self.font.render('Time: {}'.format(self.time), False, self.white)
         self.window.blit(time, (0,640))
+
+    def draw_tetrimino(self):
+        pygame.draw.rect(self.window,(self.red),[
+        self.position[0]*(self.square_height+self.margin),
+        self.position[1]*(self.square_height+self.margin),
+        self.square_width,
+        self.square_height
+        ])
         pygame.display.update()
+
+    def draw(self):
+        self.draw_background()
+        self.draw_tetrimino()
 
     def main(self):
         run = True
@@ -62,6 +82,15 @@ class Tetris:
                     run = False
                 if event.type == pygame.USEREVENT:
                     self.event_per_sec()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        if self.position[0]>0:
+                            self.position[0] -= 1
+                    if event.key == pygame.K_RIGHT:
+                        if self.position[0]<self.matrix_width-1:
+                            self.position[0] += 1
+                    if event.key == pygame.K_DOWN:
+                        self.move_down_tetrimino()
 
             self.draw()
             
